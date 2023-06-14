b73fd21faa562ad7d964db53008853fffa53c8a6
Carmen Lee <carmenlee2278@gmail.com>
2023-06-13 18:55:27 -0700

    fixing some things

---
+++ react-app/src/store/pins.js
@@ -0,0 +1,50 @@
+// ACTION CONSTANTS
+
+const GET_PINS = "pins/GET_PINS"
+const CREATE_PIN = "pins/CREATE_PIN"
+const EDIT_PIN = "pins/EDIT_PIN"
+const DELETE_PIN = "pins/DELETE_PIN"
+const CLEAR_PINS = "pins/CLEAR_PINS"
+
+// ACTION CREATORS
+
+const getPins = (pins) => ({
+    type: GET_PINS,
+    pins
+})
+
+
+// THUNKS
+
+export const getPinsThunk = () => async (dispatch) => {
+    const response = await fetch("/api/pins")
+    if (response.ok) {
+        const data = await response.json();
+        if (data.errors) {
+            return;
+        }
+        dispatch(getPins(data.pins))
+        return data.pins
+    }
+}
+
+// REDUCER
+
+const initialState = { allPins: {}, singlePin: {} };
+export default function pinsReducer(state = initialState, action) {
+    switch(action.type) {
+        case GET_PINS: {
+
+            const newState = { allPins: {}, singlePin: {} };
+            if (action.pins.length) {
+                action.pins.forEach((pin) => {
+                    newState.allPins[pin.id] = pin
+                })
+            }
+            return newState;
+        }
+        default:
+            return state
+    }
+
+}
