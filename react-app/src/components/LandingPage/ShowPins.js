f10d4d1714ee58d0c9aeff8d7818c4017a9a4bd4
Carmen Lee <carmenlee2278@gmail.com>
2023-06-13 19:47:32 -0700

    fixed a bug

--- react-app/src/components/LandingPage/ShowPins.js
+++ react-app/src/components/LandingPage/ShowPins.js
@@ -1,14 +1,20 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux'
-
+import { getPinsThunk } from '../../store/pins';
 
 export default function ShowPins() {
     const dispatch = useDispatch();
     const pinsObj = useSelector(state => state.pins.allPins)
 
+    console.log('pinsObj', pinsObj)
+
+    useEffect(() => {
+        dispatch(getPinsThunk())
+    }, [dispatch])
 
     return(
         <>
+        <h1>test</h1>
         </>
     )
 }
