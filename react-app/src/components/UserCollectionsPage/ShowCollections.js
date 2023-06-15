c609124a1317a2df2f4dcc8c1e996a83d27aea4d
Carmen Lee <carmenlee2278@gmail.com>
2023-06-15 00:42:08 -0700

    can get name of boards

--- react-app/src/components/UserCollectionsPage/ShowCollections.js
+++ react-app/src/components/UserCollectionsPage/ShowCollections.js
@@ -1,12 +1,19 @@
 import { Link } from "react-router-dom"
 import { useDispatch, useSelector } from "react-redux";
 import React, { useState, useEffect } from "react";
+import { getBoardsThunk } from "../../store/boards";
 
 
 
 export default function ShowCollections() {
 
+    const dispatch = useDispatch()
+    const boardsObj = useSelector(state => state.boards.allBoards)
+    const boardsList = Object.values(boardsObj)
 
+    useEffect(() => {
+        dispatch(getBoardsThunk())
+    },[dispatch])
 
     const sessionUser = useSelector(state => state.session.user);
     
@@ -14,6 +21,15 @@ export default function ShowCollections() {
         <>
             <Link to={`/${sessionUser.username}/created`}>Created</Link>
             <h2>this is where boards go</h2>
+            {boardsList.map((board) => (
+                <div key={board.id} className='board-card'>
+                    <Link to={`/${sessionUser.username}/${board.name}`}>
+                        <div className='card-wrapper'>
+                            <p>{board.name}</p>
+                        </div>
+                    </Link>
+                </div>
+            ))}
         </>
     )
 }
