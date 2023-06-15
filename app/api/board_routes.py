011a29d3e2003ad5c33029750efef100427c7a5e
Carmen Lee <carmenlee2278@gmail.com>
2023-06-15 15:52:07 -0700

    backend board routes set up

--- app/api/board_routes.py
+++ app/api/board_routes.py
@@ -2,7 +2,7 @@ from flask import Blueprint, jsonify, request
 from flask_login import login_required, current_user
 from app.models import Board
 from ..models.db import db
-
+from ..forms.board_form import BoardForm
 
 
 from .auth_routes import validation_errors_to_error_messages
@@ -25,7 +25,7 @@ def get_boards():
 @login_required
 def get_particular_board(id):
     """
-    Query for a board by name adn returns that board in a dictionary
+    Query for a board by name and returns that board in a dictionary
     """
     board = Board.query.get(id)
     return board.to_dict()
@@ -37,5 +37,57 @@ def post_board():
     """
     Creates a new board and returns it as a dictionary
     """
+    form = BoardForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
 
+    if form.validate_on_submit():
+        data = form.data
+        new_board = Board(
+            user_id = data['user_id'],
+            name = data['name'],
+            description = data['description']
+        )
+        db.session.add(new_board)
+        db.session.commit()
+
+        return new_board.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}
+
+
+@board_routes.route('/<int:id>', methods=['PUT'])
+@login_required
+def update_board(id):
+    """
+    Updates an existing board and returns it as a dictionary
+    """
+
+    board = Board.query.get(id)
+    form = BoardForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+
+
+    if form.validate_on_submit():
+        data = form.data
+
+        if data['title']:
+            board.name = data['name']
+        if data['description']:
+            board.description = data['description']
+
+        db.session.commit()
+        return board.to_dict()
     
+    return {'errors': validation_errors_to_error_messages(form.errors)}
+
+
+@board_routes.route('/<int:id>', methods=['DELETE'])
+@login_required
+def delete_board(id):
+    """
+    Query for a board by id and deletes board
+    """
+
+    board = Board.query.get(id)
+    db.session.delete(board)
+    db.session.commit()
+    return {"message": "Successfully deleted"}
