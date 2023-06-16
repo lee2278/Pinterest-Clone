7ec65c3763f3cbe575c78ce90de871ed5c8ab2f8
Carmen Lee <carmenlee2278@gmail.com>
2023-06-16 16:35:53 -0700

    added in some error validations

--- react-app/src/components/SignupFormModal/index.js
+++ react-app/src/components/SignupFormModal/index.js
@@ -10,38 +10,63 @@ function SignupFormModal() {
 	const [username, setUsername] = useState("");
 	const [password, setPassword] = useState("");
 	const [confirmPassword, setConfirmPassword] = useState("");
-	const [errors, setErrors] = useState([]);
+	const [errors, setErrors] = useState({});
 	const { closeModal } = useModal();
+
+
 
 	const handleSubmit = async (e) => {
 		e.preventDefault();
-		if (password === confirmPassword) {
-			const data = await dispatch(signUp(username, email, password));
-			if (data) {
-				setErrors(data);
-			} else {
-				closeModal();
-			}
+		// if (password === confirmPassword) {
+		// 	const data = await dispatch(signUp(username, email, password));
+		// 	if (data) {
+		// 		setErrors(data);
+		// 	} else {
+		// 		closeModal();
+		// 	}
+		// } else {
+		// 	setErrors([
+		// 		"Confirm Password field must be the same as the Password field",
+		// 	]);
+		// }
+		setErrors({})
+
+		const newErrors = {}
+
+		if (username.length > 30) newErrors.username = 'Username must be less than 30 characters'
+		if (username.length < 4) newErrors.username = 'Username must be greater than 4 characters'
+		if (password.length < 6) newErrors.password = 'Password must be 6 characters or more'
+		if (password !== confirmPassword) newErrors.password = 'Confirm Password field must be the same as the Password field'
+		if (email.length > 50) newErrors.email = 'Email address length is too long. Please provide a valid email address under 50 characters'
+
+
+		if (Object.values(newErrors).length) {
+			setErrors(newErrors)
+			return 
 		} else {
-			setErrors([
-				"Confirm Password field must be the same as the Password field",
-			]);
+			await dispatch(signUp(username, email, password))
+			closeModal()
 		}
+
 	};
 
 	return (
 		<>
 			<h1 id='signup-h1'>Sign Up</h1>
 			<form id='signup-modal-form' onSubmit={handleSubmit}>
-				<ul>
+				{/* <ul>
 					{errors.map((error, idx) => (
-						<li key={idx}>{error}</li>
-					))}
-				</ul>
+						<li className='sign-up-errors' key={idx}>{error}</li>
+					))} 
+				</ul> */}
+
+				{errors.username && <p className='sign-up-errors'>{errors.username}</p>}
+				{errors.email && <p className='sign-up-errors'>{errors.email}</p>}
+				{errors.password && <p className='sign-up-errors'>{errors.password}</p>}
 				<label>
 					Email
 					<input className='signup-inputs'
-						type="text"
+						type="email"
 						value={email}
 						onChange={(e) => setEmail(e.target.value)}
 						required
