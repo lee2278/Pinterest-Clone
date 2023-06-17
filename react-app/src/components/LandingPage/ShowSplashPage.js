489643370cb10972afc9e299a33cab32db707d8e
Carmen Lee <carmenlee2278@gmail.com>
2023-06-16 21:13:27 -0700

    added in some styling

--- react-app/src/components/LandingPage/ShowSplashPage.js
+++ react-app/src/components/LandingPage/ShowSplashPage.js
@@ -1,8 +1,22 @@
+import "./ShowSplashPage.css"
+
 export default function ShowSplashPage() {
 
     return(
-        <>
-        <h1>landing page</h1>
-        </>
+        <div className='splash-page-container'>
+            <div className='center-container'>
+                <h1>Get your next<br></br> weeknight dinner idea</h1>
+            </div>
+
+        <div className='pics-container'>
+            <div className='pics'>1</div>
+            <div className='pics'>2</div>
+            <div className='pics'>3</div>
+            <div className='pics'>4</div>
+            <div className='pics'>5</div>
+            <div className='pics'>6</div>
+        </div>
+
+        </div>
     )
 }
