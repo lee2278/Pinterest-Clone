028bee4fc6d6ddc005fc9ebfbfa00e7eb6602705
Carmen Lee <carmenlee2278@gmail.com>
2023-06-23 09:56:03 -0700

    some cleaning up of console logs and image conversion to aws urls

--- react-app/src/components/LandingPage/ShowSplashPage.js
+++ react-app/src/components/LandingPage/ShowSplashPage.js
@@ -18,11 +18,11 @@ export default function ShowSplashPage() {
             </div>
 
                 <div className='pics-container'>
-                    <img className='pics pic-1' src='https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1380&q=80' alt=''/>
-                    <img className='pics pic-2'src='https://images.unsplash.com/photo-1568376794508-ae52c6ab3929?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80' alt=''/>
-                    <img className='pics pic-3' src='https://images.unsplash.com/photo-1565299585323-38d6b0865b47?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1380&q=80' alt=''/>
-                    <img className='pics pic-4'src='https://images.unsplash.com/photo-1593854823322-5a737e326b1c?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=926&q=80' alt=''/>
-                    <img className='pics pic-5'src='https://images.unsplash.com/photo-1567056602606-6172dedda3ac?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1287&q=80' alt=''/>
+                    <img className='pics pic-1' src='https://capstone-pin.s3.us-west-1.amazonaws.com/a1dffbe20ebb424a863e61a349e8bc46.jpg' alt=''/>
+                    <img className='pics pic-2'src='https://capstone-pin.s3.us-west-1.amazonaws.com/d568910d03de49a9b13a538b4ef8ca38.jpg' alt=''/>
+                    <img className='pics pic-3' src='https://capstone-pin.s3.us-west-1.amazonaws.com/dee98bb5b7624ae8be9dadf732b729b9.jpg' alt=''/>
+                    <img className='pics pic-4'src='https://capstone-pin.s3.us-west-1.amazonaws.com/cda29a4fba344c2ba0b34da44004a0aa.jpg' alt=''/>
+                    <img className='pics pic-5'src='https://capstone-pin.s3.us-west-1.amazonaws.com/078c0183d45743deb71140129a2f1bc8.jpg' alt=''/>
 
                 </div>
             </div>
